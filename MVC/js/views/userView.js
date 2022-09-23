b80f41f0d0a07489ee93c3fadfad806cc5f09f92
Luiz Fernando <108633580+luizfernandodev@users.noreply.github.com>
2022-09-23 17:47:47 -0300

    Padrão MVC

---
+++ MVC/js/views/userView.js
@@ -0,0 +1,25 @@
+import { elements } from "./base.js";
+
+export const renderUser = ({
+    avatar_url,
+    htm_url,
+    public_repos,
+    followers,
+    following,
+}) => {
+    const markup = `
+    <div class="profile-header">
+        <img src="${avatar_url}" alt="Avatar">
+        <a href="${htm_url}" target="_blank">Visitar perfil</a>
+    </div>
+    <ul class="profile-list">
+        <li>Repositórios: ${public_repos}</li>
+        <li>Seguidores: ${followers}</li>
+        <li>Seguindo: ${following}</li>
+    </ul>
+
+`;
+
+elements.profile.insertAdjacentHTML("afterbegin", markup);
+
+};
